0ad9bce8e93f498a9da534e9cc29c3851fed4e4c
mpppk <niboshiporipori@gmail.com>
2017-03-02 15:42:53 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+import { loadAnimation } from 'bodymovin';
+
+class App extends Component {
+  static async get() {
+    return await axios.get('/data.json');
+  }
+
+  render() {
+    const data = App.get();
+    data.then(d => {
+      console.log(d.data);
+
+      loadAnimation({
+        container: document.getElementById("test") , // the dom element
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: d.data // the animation data
+      });
+    });
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
