4891e278990630e3f93dc05b43c25ad1d99ea852
mpppk <niboshiporipori@gmail.com>
2017-03-10 00:15:28 +0900

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import logo from './logo.svg';
 import './App.css';
+import reducer from './reducer.js';
 import JsonDropZone from './JsonDropZone.js';
+import { loadAnimation } from 'bodymovin';
+
+let store = createStore(reducer);
 
 class App extends Component {
+  constructor(){
+    super();
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(animData) {
+    loadAnimation({
+      container: document.getElementById("test") , // the dom element
+      renderer: 'canvas',
+      loop: true,
+      autoplay: true,
+      animationData: animData // the animation data
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>AE2WEB</h2>
+      <Provider store={store}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>AE2WEB</h2>
+          </div>
+          <JsonDropZone onDrop={this.onDrop}/>
         </div>
-        <JsonDropZone/>
-      </div>
+      </Provider>
     );
   }
 }
--- src/JsonDropZone.js
+++ src/JsonDropZone.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 
-import { loadAnimation } from 'bodymovin';
-
 class JsonDropZone extends Component {
   constructor() {
     super();
     this.onDrop = this.onDrop.bind(this);
-    this.state = {};
     this.reader = this.createFileReader();
   }
 
   createFileReader() {
     let reader = new FileReader();
-    reader.onload = () => this.setState({animData: JSON.parse(reader.result)});
+    reader.onload = () => this.props.onDrop(JSON.parse(reader.result));
     return reader;
   }
 
@@ -22,14 +19,6 @@ class JsonDropZone extends Component {
   }
 
   render() {
-    loadAnimation({
-      container: document.getElementById("test") , // the dom element
-      renderer: 'canvas',
-      loop: true,
-      autoplay: true,
-      animationData: this.state.animData // the animation data
-    });
-
     return (
         <Dropzone onDrop={this.onDrop} accept="application/json">
         </Dropzone>
